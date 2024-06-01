3e8cd2167ec15039727fd8888a86ad61d8eccee9
Incognito0989 <andrew109jones@gmail.com>
2024-06-01 16:38:20 -0400

    Add next hand button

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,15 +1,18 @@
 import { Dispatch, SetStateAction, useState } from 'react'
 
-import { BlackjackHand, Card } from '../../interfaces/card.interface'
+import { BlackjackHand } from '../../interfaces/card.interface'
+import { dealEmptyHand, dealHand } from '../../services/deck'
 import calculateHandOfCardsTotal from '../../services/handOfCardsCalculation'
 import { Dealer } from '../dealer/dealer'
+import { EndGameActions } from '../end-game-actions/end-game-actions'
 import { Player } from '../player/player'
 import { Title } from '../title/title'
 import * as styles from './app.module.css'
 
 interface AppState {
     playerFinalTotals: number[]
-    dealerHand: Card[]
+    dealerHand: BlackjackHand
+    handCountIndex: number
 }
 
 export function App() {
@@ -18,7 +21,8 @@ export function App() {
         Dispatch<SetStateAction<AppState>>,
     ] = useState({
         playerFinalTotals: [],
-        dealerHand: [],
+        dealerHand: dealEmptyHand(),
+        handCountIndex: 0,
     })
 
     function notifyDealerToPlay(
@@ -34,10 +38,21 @@ export function App() {
         })
     }
 
-    function setDealerFinalHandOfCards(dealerFinalHandOfCards: Card[]): void {
+    function setDealerFinalHandOfCards(
+        dealerFinalHandOfCards: BlackjackHand
+    ): void {
         setAppState({
             ...appState,
             dealerHand: dealerFinalHandOfCards,
+        })
+    }
+
+    function nextHand(): void {
+        setAppState({
+            ...appState,
+            playerFinalTotals: [],
+            dealerHand: dealEmptyHand(),
+            handCountIndex: appState.handCountIndex + 1,
         })
     }
 
@@ -46,14 +61,19 @@ export function App() {
             <div className={styles.game}>
                 <Title />
                 <Dealer
+                    key={`dealer_${appState.handCountIndex}`}
                     playerFinalTotals={appState.playerFinalTotals}
                     onHasFinishedPlaying={setDealerFinalHandOfCards}
                 />
                 <Player
+                    key={`player_${appState.handCountIndex}`}
                     name="PLAYER"
                     onHasFinishedActions={notifyDealerToPlay}
                     dealerHand={appState.dealerHand}
                 />
+                {appState.dealerHand.finished && (
+                    <EndGameActions onNewHand={nextHand} />
+                )}
             </div>
         </>
     )
